ac1ac94ebab9310e6816dcad92196c906270978c
Helga <levchenkohelga@gmail.com>
2023-11-06 21:34:29 +0200

    api errors

--- app/calendarificApi.js
+++ app/calendarificApi.js
@@ -6,6 +6,9 @@ const API_KEY = "pSAlqM4GMzkfE7RyfpUukFAaFhgjLFHJ";
 export async function loadCountries() {
     const response = await fetch(`${API_URL}countries?api_key=${API_KEY}`);
     const countries = await response.json();
+    if (!Array.isArray(countries.response.countries)) {
+        throw new Error("Countries is not array");  
+    };
     return countries.response.countries;
 }
 export async function loadHolidays(country, year) {
@@ -13,5 +16,8 @@ export async function loadHolidays(country, year) {
         `${API_URL}holidays?api_key=${API_KEY}&country=${country}&year=${year}`
     );
     const holidays = await response.json();
+    if (!Array.isArray(holidays.response.holidays)) {
+        throw new Error("Holidays is not array");  
+    };
     return holidays.response.holidays;
 }
